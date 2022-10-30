e6422d872feacb91afd8f20e32d53af6a0371c7f
miru25 <r202202276pj@jindai.jp>
2022-10-30 18:51:08 +0900

    20221030

---
+++ de12/python/work1.py
@@ -0,0 +1,8 @@
+name="yuri"
+waist=66
+age=29
+
+#nameという変数にyuriを代入している
+#waistという変数に66を代入している
+#ageという変数に29を代入している
+print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。")
---
+++ de12/python/work6.py
@@ -0,0 +1,32 @@
+
+import random
+from re import I
+from tkinter import Y
+
+
+counter=0   #最初の数字は0
+
+print("99を超えないように限界まで挑戦してください。")
+print("現在の数字は0です。")
+
+while counter<=99:               #上限の99を超えるまで同じ流れが繰り返される
+    a = random.randint(1,30)           #ランダムで足される数字の幅が1~30
+    question=input("まだ足しますか。")
+    
+    if question=="yes":
+        counter=counter+a       #ランダムに数字が足される
+        print("現在の合計は",counter,"です。")      #合計が表示される
+    
+    elif counter==99:       #合計が99ぴったりだと以下の文が表示され、繰り返しから抜ける
+        print("おめでとうございます。99ぴったりです。")
+        break
+    
+    elif question=="no":        #noと入力されるとハイスコアが表示され、繰り返しから抜ける
+        print("ハイスコアは",counter,"です。")
+        break
+    
+
+else:       #合計が99を超えると以下の文が表示される
+    print("99を超えました。失敗です。")
+
+
